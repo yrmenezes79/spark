515526a7997c7c9040b676ee8922918813f0155c
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-08-28 16:55:45 -0300

    Create analise_vendas.py

---
+++ analise_vendas.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, desc, sum # Importando a função 'sum'
+
+spark = SparkSession.builder.appName("AnaliseDeVendas").getOrCreate()
+
+df = (spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("vendas.csv"))
+
+receita_df = df.withColumn("Receita", col("Preco") * col("Quantidade"))
+
+# Lógica de agregação corrigida
+receita_por_categoria = (receita_df.groupBy("Categoria")
+                                   .agg(sum("Receita").alias("Receita_Total"))
+                                   .orderBy(desc("Receita_Total")))
+
+print("--- Receita Total por Categoria ---")
+receita_por_categoria.show()
+
+spark.stop()
