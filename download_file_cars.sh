9be7809ac2ada045bcd2226e70f31e9f824e7c6b
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2025-09-17 22:37:34 -0300

    Create download_file_cars.sh

---
+++ download_file_cars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -L -o mock-dataset-of-second-hand-car-sales.zip\
+  https://www.kaggle.com/api/v1/datasets/download/msnbehdani/mock-dataset-of-second-hand-car-sales
+
+unzip mock-dataset-of-second-hand-car-sales.zip
